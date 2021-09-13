752cd71694154028816c3f979a2567156f17e247
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2021-09-12 20:15:53 -0400

    Setup Tests

---
+++ src/utils/trends.spec.ts
@@ -0,0 +1,21 @@
+import { determineTrend } from ".";
+
+describe("determineTrend()", () => {
+  it("identifies an increase in price", () => {
+    const result = determineTrend(1, 2);
+
+    expect(result).toBe("up");
+  });
+
+  it("identifies a decrease in price", () => {
+    const result = determineTrend(2, 1);
+
+    expect(result).toBe("down");
+  });
+
+  it("identifies same prices", () => {
+    const result = determineTrend(1, 1);
+
+    expect(result).toBe("same");
+  });
+});
