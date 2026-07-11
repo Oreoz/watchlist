daad7a3be636f665ef0b4b9a758ce15f5fa48e64
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2026-07-10 20:07:56 -0400

    chore: remove coverage configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      enabled: true,
       provider: "v8",
       reporter: ["text", "json-summary", "json"],
       reportOnFailure: true,
