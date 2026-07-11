ea77a1de01853c7be3d2c77631295053cffb92f4
Jean-Philippe Roy <ohnoesmyoreos@gmail.com>
2026-07-10 20:06:27 -0400

    build: upgrade to node 26

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
+      enabled: true,
       provider: "v8",
       reporter: ["text", "json-summary", "json"],
       reportOnFailure: true,
+    },
+    env: {
+      NODE_OPTIONS: "--no-webstorage",
     },
     globals: true,
   },
